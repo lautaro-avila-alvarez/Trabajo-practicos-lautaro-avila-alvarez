9da93e128f3e1591f9946f5b4870dc5bc5a9dcbb
alumno <51316311@ivt.edu.ar>
2026-06-04 10:47:21 -0300

    jhgfkyfryr

---
+++ tp5/app.js
@@ -0,0 +1,41 @@
+//declarar variable
+ let numero1
+ let numero2
+ let nombreAlumno 
+ let apellidoAlumno
+ let edadAlumno
+ let precio1
+ let precio2
+ let precio3
+ let=comidaFavorita
+ let=colorFavorito
+ let=numeroFavorito
+ //asignar valor
+ numero1=67
+ numero2=40
+ nombreAlumno="chatGPT"
+ apellidoAlumno="motosiera"
+ edadAlumno=13
+ precio1=32
+ precio2=40
+ precio3=67
+ comidaFavorita="capeletines verdes"
+ colorFavorito="azul"
+ numeroFavorito="67"
+ //declarar y asignar al mismo tiempo
+ suma=numero1+numero2
+ resta=numero1-numero2
+ Multiplicación=numero1*numero2
+ División=numero1/numero2
+ total=precio+precio2+precio3
+ //Mostrar la información
+ console.log("el resultado de Multiplicación es:"+Multiplicación)
+ console.log("el resultado de División es:"+División)
+alert("el resultado de la suma es:"+ suma)
+alert("el resultado de la resta es:"+ resta)
+
+//ejercicio7
+let pais= "argentina"
+console.log(pais)
+pais="brasil"
+console.log(pais)
